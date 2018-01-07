7670da91f9e711c4c156475a240181f44fa24021
Wei-Ming <barry800414@gmail.com>
2018-01-07 21:24:00 +0800

    change to use HashHistory for github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // Absolute imports
 import React from 'react';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 
 import routes from './routes';
 
 
 export default function () {
   return (
-    <Router routes={routes} history={browserHistory}/>
+    <Router routes={routes} history={hashHistory}/>
   );
 }
