cb5cddeb1384790c480bed374842c11dfae8b9c3
Wei-Ming <barry800414@gmail.com>
2018-01-28 23:58:59 +0800

    add ReminderList to view and manage reminders

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import BlackRoot from './Root/BlackRoot';
 import WhiteRoot from './Root/WhiteRoot';
 import TotalWorkingTime from './TotalWorkingTime';
 import CheckInList from './CheckInList';
+import ReminderList from './ReminderList';
 
 export default (
     <div>
@@ -13,6 +14,7 @@ export default (
         </Route>
         <Route component={WhiteRoot}>
             <Route path="/check-ins/:key" component={CheckInList} />
+            <Route path="/reminders/:key" component={ReminderList} />
         </Route>
     </div>
 );
